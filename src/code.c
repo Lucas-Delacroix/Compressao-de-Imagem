764fa33feaeefb00f71c5fb0e2578e8a66dd5481
Lucas-Delacroix <delacroixdev@gmail.com>
2024-09-15 20:17:26 -0300

    Atualiza comentarios explicativos

--- src/code.c
+++ src/code.c
@@ -7,23 +7,34 @@
 
 int main(int argc, char *argv[])
 {
+	//Declarar variavel img do tipo struct pertecente a pgm.h
 	struct pgm img;
+
+	//Verificar argumentos de execução
 	if (argc != 3)
 	{
 		printf("Formato: \n\t %s <imagemEntrada.pgm> <imagemSaida.pgm>\n", argv[0]);
 		exit(1);
 	}
 
+	//leitura da imagem PGM do arquivo de entrada
 	readPGMImage(&img, argv[1]);
 
+	// compressImage(&img);
+
+	//Declaração de um ponteiro root do tipo quadtree (nó) e criação da imagem representada por img
 	QuadTreeNode *root = construirQuadTree(&img, 0, 0, img.largura, img.altura);
 
-	compressQuadTree(root, "saida.bin");
+	//Comprimir quadtree e salva os dados como saida.bin 
+	compressQuadTree(root, img.largura, img.altura, "saida.bin");
 
+	//Visualizar estrutura da quadtree
 	imprimirQuadTree(root);
 
+	//Gravar a imagem img no arquivo de saída no segundo argumento de entrada
 	writePGMImage(&img, argv[2]);
 
+	//Exibir PGM na tela.
 	viewPGMImage(&img);
 
 	return 0;
--- src/pgm.c
+++ src/pgm.c
@@ -3,20 +3,21 @@
 #include <stdlib.h>
 
 
-
+//Função para ler uma imagem PGM e armazenar os dados na struct pgm
 void readPGMImage(struct pgm *pio, char *filename){
 
-
+	//Criação do ponteiro para um arquivo fp(manipular o arquivo da imagem PGM)
 	FILE *fp;
 	char ch;
 
 
+	//Tenta abrir o código com o nome filename para leitura(r), se não conseguir imprime erro e termina
 	if (!(fp = fopen(filename,"r"))){
 		perror("Erro.");
 		exit(1);
 	}
 
-
+	//Verificar formato do arquivo (se é pgm ou não), se não conseguir printa a mensagem e termina 
 	if ( (ch = getc(fp))!='P'){
 		puts("A imagem fornecida não está no formato pgm");
 		exit(2);
@@ -26,34 +27,43 @@ void readPGMImage(struct pgm *pio, char *filename){
 	
 	fseek(fp,1, SEEK_CUR);
 
-
+	//Ignora os comentários # no arquivo PGM, continua lendo ate que não encontre linhas com #
 	while((ch=getc(fp))=='#'){
 		while( (ch=getc(fp))!='\n');
 	}
 
-
+	//garantir que a leitura continue no lugar certo, volta depois de ler o último caractere não comentado
 	fseek(fp,-1, SEEK_CUR);
 
-
+	//ler a altura e a largura e armazenar os valores nos campos altura e largura da struct pgm
 	fscanf(fp, "%d %d",&pio->altura,&pio->largura);
+
+	//Verificar se houve algum erro na leitura do arquivo, se ocorrer ele termina
 	if (ferror(fp)){ 
 		perror(NULL);
 		exit(3);
 	}	
+
+	//ler o valor máximo (mv) que pode ser ate 255 
 	fscanf(fp, "%d",&pio->mv);
+
+	//ignorar caractere
 	fseek(fp,1, SEEK_CUR);
 
-
+	//Alocação de memoria de cada pixel com exatidão
 	pio->pData = (unsigned char*) malloc(pio->largura * pio->altura * sizeof(unsigned char));
 
 
 	switch(pio->tipo){
+		//Se o tipo for em imagem de texto ele indentifica e lê os valores em formato de texto com fscan e são armazenados em pData
+
 		case 2:
 			puts("Lendo imagem PGM (dados em texto)");
 			for (int k=0; k < (pio->largura * pio->altura); k++){
 				fscanf(fp, "%hhu", pio->pData+k);
 			}
 		break;	
+		//Se o tipo for em binário ele lê os pixel diretamente do arquivo em formato binario com o fread
 		case 5:
 			puts("Lendo imagem PGM (dados em binário)");
 			fread(pio->pData,sizeof(unsigned char),pio->largura * pio->altura, fp);
@@ -62,6 +72,7 @@ void readPGMImage(struct pgm *pio, char *filename){
 			puts("Não está implementado");
 	}
 	
+	//fechar arquivo
 	fclose(fp);
 
 
