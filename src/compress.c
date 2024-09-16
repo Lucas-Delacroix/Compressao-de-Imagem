88dee9b127d60f48ceed5f60f10e57c5ed3dd59c
Lucas-Delacroix <delacroixdev@gmail.com>
2024-09-16 01:53:46 -0300

    Atualiza codigo de compressao

--- src/compress.c
+++ src/compress.c
@@ -2,20 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int buffer = 0;
-static int bitsDisponiveis = 8;
+static int buffer = 0; //Vai armazenar os bits temporários que serão escritos no arquivo
+static int bitsDisponiveis = 8; //quantos bits ainda podem ser armazenados no buffer antes de precisar escrever os dados no arquivo.
+//Neste caso um byte
 
 
 // Função para gerar o arquivo binário
 void compressQuadTreeToBin(QuadTreeNode *root, int largura, int altura, const char *filename)
 {
-    FILE *file = fopen(filename, "wb");
+    
+    FILE *file = fopen(filename, "wb");// Com "wb" abre o arquivo para escrita, truncando-o se já existir.
     if (!file)
     {
         perror("Erro ao abrir arquivo de bitstream");
         exit(1);
     }
-
+    //Escreve no arquivo o valor no endereço de largura ou altura, aloca tamanho de inteiro e adiciona '1' elemento, largura ou altura
+    //ao arquivo
     fwrite(&largura, sizeof(int), 1, file);
     fwrite(&altura, sizeof(int), 1, file);
 
@@ -26,7 +29,7 @@ void compressQuadTreeToBin(QuadTreeNode *root, int largura, int altura, const ch
     printf("Compressão binária completa. Arquivo salvo como %s\n", filename);
 }
 
-// Função para gerar um arquivo legível em texto com o bitstream
+// Função que vai gerar um arquivo legível em texto com o bitstream
 void compressQuadTreeToTxt(QuadTreeNode *root, int largura, int altura, const char *filenameTxt)
 {
     FILE *fileTxt = fopen(filenameTxt, "w");
@@ -44,7 +47,7 @@ void compressQuadTreeToTxt(QuadTreeNode *root, int largura, int altura, const ch
     printf("Compressão para texto completa. Arquivo salvo como %s\n", filenameTxt);
 }
 
-// Função original para escrita em binário
+// Função para escrever bitstream em binário
 void QuadTreeParaBitstream(QuadTreeNode *node, FILE *file)
 {
     if (node->folha)
@@ -82,13 +85,20 @@ void QuadTreeParaTxtBitstream(QuadTreeNode *node, FILE *fileTxt)
 // Função de escrita de bits no arquivo binário
 void escreverBits(FILE *file, int valor, int numBits)
 {
-
+    //A função decide se escreve todos os bits que foram passados ou apenas uma parte
+    //Se o buffer já tenha alguns bits e esteja parcialmente preenchido.
     while (numBits > 0)
     {
         int bitsParaEscrever = (numBits < bitsDisponiveis) ? numBits : bitsDisponiveis;
+        //Desloca os bits no buffer para a esquerda, 
+        //Desloca o valor original para a direita
+        // | (pipe) Combina o buffer com os novos bits que serão escritos.
         buffer = (buffer << bitsParaEscrever) | (valor >> (numBits - bitsParaEscrever));
         bitsDisponiveis -= bitsParaEscrever;
         numBits -= bitsParaEscrever;
+        
+        //garante que apenas os bits restantes do valor serão considerados na próxima iteração. 
+        //Zeran todos os bits acima de numBits.
         valor &= (1 << numBits) - 1;
 
         if (bitsDisponiveis == 0)
@@ -100,7 +110,7 @@ void escreverBits(FILE *file, int valor, int numBits)
     }
 }
 
-// Finaliza o bitstream
+//Vai garantir que os bits que restaram no buffer sejam escritos no arquivo.
 void finalizarBitstream(FILE *file)
 {
 
